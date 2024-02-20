e2102baf9d0e7eeff143d79a860e2a4798364fcb
IDGS-802-21002392 <dianaHdez0119@gmail.com>
2024-02-19 20:52:51 -0600

    DLHR 19/02/2024 Variable global

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Response
 import forms 
+from flask_wtf.csrf import CSRFProtect
+from flask import flash
+from flask import g
+
 app = Flask(__name__)
+app.secret_key ='esta es la clave secreta'
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.before_request
+def before_request():
+    g.prueba ='Hola'
+    print('antes de ruta')
+
 @app.route("/alumnos", methods=["GET", "POST"])
 def index1():
+    print('dentro de alumnos')
+    valor = g.prueba
+    print('El dato es: {}'.format(valor))
     nom=""
     email=""
     apaterno=""
     alum_form = forms.UserForm(request.form)
-    if request.method=='POST':
+    if request.method=='POST' and alum_form.validate():
         nom=alum_form.nombre.data
         email=alum_form.email.data
         apaterno=alum_form.apaterno.data
+        mensaje = 'Bienvenido: {}'.format(nom)
+        flash(mensaje)
         print("nombre:{}".format(nom))
         print("email:{}".format(email))
         print("apellido:{}".format(apaterno))
     return render_template("alumnos.html", form= alum_form, nom=nom, email=email, apaterno=apaterno)
+
+@app.after_request
+def after_request(response):
+    print('despues de ruta 3')
+    return response
 
 @app.route("/maestros")
 def index2():
